df92003925e9fc0138cd3133ce06abb630cf78c5
kmj4875 <minjae.kim@wustl.edu>
2016-04-18 18:48:50 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -9,11 +9,12 @@ function question() {
   // your code goes here
     order = prompt("What would you like to order? burrito bowl tacos?");
     toppings = prompt("What meat toppings would you like? steak carnitas chicken barbacoa sofritas");
-        toppings = prompt("What rice or veggie topping would you like?");
+       riceveggies = prompt("What rice or veggie topping would you like?");
+        sauces = prompt("What sauce would you like?");
 }
 
 // call the QUESTION function with the order options
 question();
 
 // output their order
-document.write("<p>You ordered a " + order + " with " + toppings + "<p>");
+document.write("<p>You ordered a " + order + " with " + toppings + riceveggies + sauces "<p>");
