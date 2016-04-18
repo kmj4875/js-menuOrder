e1b350eb4dd2b6cb2347a41d70668f005b0aa0be
kmj4875 <minjae.kim@wustl.edu>
2016-04-18 18:29:37 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,12 +4,23 @@ var order = '';
 
 // build a function to ask questions
 
-function question() {
+function question(questionText,) {
+    var ansInput = promt (questionText)
+    
   // your code goes here
+if (ansInput === yes){
+  order = ""
 }
+  
+}
+order = "mainFood"
 
 // call the QUESTION function with the order options
-question();
+question("would you like some lettuce?");
+question("would you like some salsa?");
+question("would you like some tomato?");
+question("would you like some rice?");
+
 
 // output their order
 document.write("<p>You ordered: " + order + "<p>");
